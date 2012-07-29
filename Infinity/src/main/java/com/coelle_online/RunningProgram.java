17c32cafdc73621bcc7e7fdd153b364f964b37b2
niels at home <ncoelle@gmx.de>
2012-07-29 05:36:30 +0200

    * Moved the running flag into an inner class

--- Infinity/src/main/java/com/coelle_online/RunningProgram.java
+++ Infinity/src/main/java/com/coelle_online/RunningProgram.java
@@ -1,16 +1,42 @@
 package com.coelle_online;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
 @SuppressWarnings("UseOfSunClasses")
 public class RunningProgram implements SignalHandler {
-    private boolean running = true;
+    /*
+     * Our first task, was to remove direct access to the running flag
+     * since the class modified it in two places. Sprout an inner class,
+     * and simply delegate to getters and setters.
+     *
+     * Moving the running flag to a separate class gives us a number of
+     * benefits. It lets us hide the implementation of how we handle
+     * running, and puts us down the road of clearly teasing apart the
+     * overloaded responsibilities.
+     * [http://www.thekua.com/atwork/2009/02/controlling-time-how-to-deal-with-infinity/]
+     */
+    private static class RunningCondition {
+        private boolean running = true;
+
+        public boolean shouldContinue() {
+            return running;
+        }
+
+        public void stop() {
+            running = false;
+        }
+    }
+
+    private RunningCondition condition = new RunningCondition();
 
     @Override
     public final void handle(final Signal signal) {
         // Program terminated by a signal
-        running = false;
+        condition.stop();
     }
 
     public static void main(String[] args) {
@@ -22,11 +48,38 @@ public class RunningProgram implements SignalHandler {
     }
 
     private void start() {
-        running = true;
-        while (running) {
+        while (condition.shouldContinue()) {
             // do some work (that we want to unit test)
             // it changes about ten fields depending on what condition
             // gets executed
         }
     }
+
+    @Override
+    public final String toString() {
+        //noinspection ChainedMethodCall
+        return new ToStringBuilder(this).
+                append("condition", condition).
+                toString();
+    }
+
+    @Override
+    public final int hashCode() {
+        //noinspection ChainedMethodCall
+        return new HashCodeBuilder().append(condition).toHashCode();
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RunningProgram other = (RunningProgram) obj;
+        //noinspection ChainedMethodCall
+        return new EqualsBuilder().append(this.condition, other.condition).isEquals();
+    }
+
 }
